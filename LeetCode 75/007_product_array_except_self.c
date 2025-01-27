250c17933ca9cbb3bee38cecda25049bc28bd2cd
theflyoccultist <y.delahaije@gmail.com>
2025-01-27 21:58:51 +0100

    challenge 7

---
+++ LeetCode 75/007_product_array_except_self.c
@@ -0,0 +1,28 @@
+// Product of array except self
+
+int* productExceptSelf(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    int* left = (int*)malloc(numsSize * sizeof(int));
+    int* right = (int*)malloc(numsSize * sizeof(int));
+    
+    left[0] = 1;
+    right[numsSize - 1] = 1;
+
+    for (int i = 1; i < numsSize; i++) {
+        left[i] = left[i - 1] * nums[i - 1];
+    }
+
+    for (int i = numsSize - 2; i >=0; i--) {
+        right[i] = right[i + 1] * nums[i + 1];
+    }
+
+    for (int i = 0; i < numsSize; i++) {
+        result[i] = left[i] * right[i];
+    }
+
+    free(left);
+    free(right);
+
+    *returnSize = numsSize;
+    return result;
+}
